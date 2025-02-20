2f1d2d461a614e2d7567393270a557d26436d87e
Mybro007 <Mybro007@mail.ru>
2025-02-20 23:11:36 +0300

    Homework 3 v.2

--- HW3/mainwindow.cpp
+++ HW3/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    newWindow = new Dialog(this);
+    newWindow->show();
 }
 
 MainWindow::~MainWindow()
